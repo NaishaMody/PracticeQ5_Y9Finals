c3bcd08c4dc53e6adb69789db10f30f79845a519
naish <naish@SURFACE-LAPTOP->
2025-06-20 19:47:32 +0100

    Add project files.

---
+++ PracticeQ5_Y9Finals/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PracticeQ5_Y9Finals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int e1 = 100;
+            int e2 = 100;
+            int e3 = 100;
+            Console.WriteLine("Please enter the number of essays handed in late");
+            int late = Convert.ToInt32(Console.ReadLine());
+            int TotalMark = e1 + e2 + e3;
+            if (late == 1)
+            {
+                TotalMark = TotalMark - 10;
+            }
+            else if (late > 1)
+            {
+                TotalMark = TotalMark / 2;
+            }
+
+            if (TotalMark <= 0)
+            {
+                TotalMark = 0;
+            }
+            Console.WriteLine("The total mark is: " + TotalMark);
+
+        }
+    }
+}
